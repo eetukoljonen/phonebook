c9dc33d751e97d1a42430114dd2803acbd64f52d
Eetu Koljonen <eetu.koljonen@gmail.com>
2024-07-16 14:56:24 +0300

    3.5 done

--- index.js
+++ index.js
@@ -55,6 +55,36 @@ app.get('/info', (request, response) => {
 	)
 })
 
+function generateId() {
+	return String(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER))
+}
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body
+
+	if (!body.name) {
+		return response.status(400).json({ 
+			error: 'name missing' 
+		})
+	}
+
+	if (persons.find(person => person.name === body.name)) {
+		return response.status(400).json({ 
+			error: `${body.name} already added`
+		})
+	}
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: generateId()
+	}
+
+	persons = persons.concat(person)
+
+	response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
