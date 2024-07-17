59308a0a4be38f2b2151562fc74e575c1dd8017d
Eetu Koljonen <eetu.koljonen@gmail.com>
2024-07-17 15:52:06 +0300

    3.13 done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const morgan = require('morgan')
 const express = require('express')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 app.use(express.json())
@@ -34,7 +36,9 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
 
 app.get('/api/persons/:id', (request, response) => {
